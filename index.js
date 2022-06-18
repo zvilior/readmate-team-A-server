c38dc1225e588cda0212a15f440b43f53e5fb21a
Oren Argaman <oren1219@gmail.com>
2022-06-18 16:33:14 +0300

    fixed typo's

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const express = require('express'),
-    app = express(),
-    PORT = 3001;
+const express = require("express"),
+  app = express(),
+  PORT = 3001;
 
-const router = require('./Routs')
-app.use(express.json()) 
-app.use(require('cors')())
-app.use("/api",router)
+const router = require("./Routers");
+app.use(express.json());
+app.use(require("cors")());
+app.use("/api", router);
 
-app.listen(PORT, () => console.log(`server is running => ${PORT}`))
-require('./DataAcsesLayer/db').connect();
+app.listen(PORT, () => console.log(`server is running => ${PORT}`));
+require("./DL/db").connect();
