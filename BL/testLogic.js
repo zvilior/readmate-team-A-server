aa6b42ff1b05d50c4fb3adeff2f73adce7bbf85d
efrat5454 <efrat5454@gmail.com>
2022-06-29 20:05:13 +0300

    getTestForUser

--- BL/testLogic.js
+++ BL/testLogic.js
@@ -1,1 +1,17 @@
-const testController = require("DL/controllers/testController.js");
+const testController = require("../DL/controllers/testController.js");
+const userController = require("../DL/controllers/userController.js");
+const userTestController = require("../DL/controllers/userTestController.js");
+
+const getTestForUser = async (userId) => {
+  const user = await userController.read({ _id: userId });
+  const level = user.level;
+  let allTestsLevel = await testController.read({ level });
+  allTestsLevel = allTestsLevel.map((x) => x._id);
+  let testCompleted = await userTestController.read({ userId });
+  testCompleted = testCompleted.map((x) => x.userId);
+  const tests = allTestsLevel.filter(
+    (testId) => !testCompleted.include(testId)
+  );
+  let testId = tests[Math.floor(Math.random() * tests.length)];
+  return await testController.read({ _id: testId });
+};
